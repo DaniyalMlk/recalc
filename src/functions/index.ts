b173754c892602fa8918f21692b7795f48cb84ae
Daniyal <103329154+DaniyalMlk@users.noreply.github.com>
2026-08-30 02:35:44 +0000

    feat: add financial function pack with TVM, DCF, depreciation and date serials

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -5,5 +5,8 @@ import "./logical.js";
 import "./text.js";
 import "./lookup.js";
 import "./info.js";
+import "./financial.js";
 
 export * from "./registry.js";
+export { solveRate } from "./solver.js";
+export { dateToSerial } from "./financial.js";
